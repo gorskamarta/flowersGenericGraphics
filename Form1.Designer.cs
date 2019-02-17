bea9ce2d8eaa21a72c4990db5dee65aef9152e02
Marta Górska <marta.gradzielewska@gmail.com>
2019-02-17 15:00:56 +0100

    Version 1 - Basic generation of a flower with petals

---
+++ Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace GrafikaGenryczna
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Generuj = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Generuj
+            // 
+            this.Generuj.Location = new System.Drawing.Point(942, 791);
+            this.Generuj.Name = "Generuj";
+            this.Generuj.Size = new System.Drawing.Size(300, 150);
+            this.Generuj.TabIndex = 0;
+            this.Generuj.Text = "Generuj";
+            this.Generuj.UseVisualStyleBackColor = true;
+            this.Generuj.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1254, 953);
+            this.Controls.Add(this.Generuj);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Generuj;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GrafikaGenryczna
+{
+    public partial class Form1 : Form
+    {
+        private Graphics ob;
+        System.Drawing.Pen Pen;
+        double alfa = 0.0;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            ob = this.CreateGraphics();
+            Pen = new System.Drawing.Pen(System.Drawing.Color.BlueViolet);
+            //ob.DrawEllipse(Pen, new Rectangle(270, 206, 50, 50));
+            //ob.DrawEllipse(Pen, new Rectangle(245, 181, 100, 100));
+            //ob.DrawEllipse(Pen, new Rectangle(220, 156, 150, 150));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ob.DrawEllipse(Pen, new Rectangle(270, 206, 50, 50));
+            
+            drawPetal(ob, Pen, alfa);
+            alfa = alfa + 0.5;
+        
+        }
+
+        private void drawPetal(Graphics ob, System.Drawing.Pen Pen, double angle)
+        {
+            // współrzędne środka okręgów
+            int a = 295;
+            int b = 231;
+
+            //promienie okręgów
+            int r1 = 25;
+            int r2 = 50;
+            int r3 = 75;
+
+            double angle2 = angle - 0.25;
+            double angle3 = angle + 0.25;
+
+            double cosinusAngle = Math.Cos(angle);
+            double sinusAngle = Math.Sin(angle);
+
+            System.Drawing.PointF point2 = new PointF((int)((double)a + (double)r1 * cosinusAngle), (int)((double)b + (double)r1 * sinusAngle));
+            System.Drawing.PointF point3 = new PointF((int)((double)a + (double)r2 * Math.Cos(angle2)), (int)((double)b + (double)r2 * Math.Sin(angle2)));
+            System.Drawing.PointF point4 = new PointF((int)((double)a + (double)r2 * Math.Cos(angle3)), (int)((double)b + (double)r2 * Math.Sin(angle3)));
+            System.Drawing.PointF point5 = new PointF((int)((double)a + (double)r3 * cosinusAngle), (int)((double)b + (double)r3 * sinusAngle));
+            System.Drawing.PointF[] points = { point2, point3, point5, point4 };
+            ob.DrawPolygon(Pen, points);
+        }
+    }
+}
